c0b614c10b0e0b0b06a775bdd69d360f87eeb0da
Sam Hill <samuhill@gmail.com>
2020-12-16 12:31:25 -0500

    more logging

--- mesh-fixer.js
+++ mesh-fixer.js
@@ -45,26 +45,37 @@ module.exports = function (cells, positions, _options) {
   } else {
     tolerance = calcTolerance(cells, positions)
   }
+  console.log("tolerance:", tolerance);
 
   var GRID_SIZE = 2 * tolerance
 
+  console.log("starting contour");
   var mesh = contour(rasterize(cells, positions, {
     resolution: 0.5 * tolerance
   }))
+  console.log("contour complete");
 
+  console.log("creating grid");
   var grid = createGrid(cells, positions, GRID_SIZE)
+  console.log("grid complete");
 
+  console.log("calculating normals");
   mesh.normals = calcNormals(mesh.cells, mesh.positions)
+  console.log("finished calculating normals");
+
+  console.log("refining mesh");
   mesh = refine(mesh.cells, mesh.positions, mesh.normals, {
     edgeLength: 0.5 * tolerance
   })
+  console.log("mesh complete");
 
+  console.log("projecting verts");
   projectVerts(
     grid,
     positions,
     mesh.positions,
     mesh.normals,
     GRID_SIZE)
-
+  console.log("complete");
   return mesh
 }
